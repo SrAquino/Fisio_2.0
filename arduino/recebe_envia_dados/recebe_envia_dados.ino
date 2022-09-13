73993dbd3ff47ea881a52c74e33da4279a07cea0
SrAquino <douglasaquino4@gmail.com>
2022-09-13 17:07:44 -0300

    debug do arduino

--- arduino/recebe_envia_dados/recebe_envia_dados.ino
+++ arduino/recebe_envia_dados/recebe_envia_dados.ino
@@ -23,14 +23,11 @@ void loop() {
   
   if(Serial2.available()){
 
-    //Usando readString()
-
     String leituraBuffer = Serial2.readStringUntil('\n');
-
     int tamanhoString = leituraBuffer.length();
 
-//    Serial.print("Tamanho: ");
-//    Serial.println(tamanhoString);
+    Serial.print("Tamanho: ");
+    Serial.println(tamanhoString);
 
     if(tamanhoString == 2){
       if(leituraBuffer == "3\r"){
@@ -49,75 +46,14 @@ void loop() {
     }else if(tamanhoString == 6){
       Serial.println(leituraBuffer);
     }
-    
-    //Usando serialByte()
-//    int rlen = Serial2.readBytes(buf, BUFFER_SIZE);
-//    Serial2.flush();
-//
-//    //prints the received data
-////    Serial.print("Tamanho do buffer: ");
-////    Serial.println(rlen);
-////    for(int i = 0; i < rlen; i++){
-////      Serial.print(buf[i]);
-////    }
-//    if(rlen == 3){
-//      if(buf[0] == '3'){
-//      digitalWrite(53, HIGH);
-//      delay(100);
-//      digitalWrite(53, LOW);
-//        Serial2.write("4");
-//      }else{
-//        Serial.write(buf[0]);
-//      }
-//    }else if(rlen == 4){ 
-//        Serial.write("P;\n");
-//    }else if(rlen == 7){
-////        Serial.write(buf[0] + buf[1] + buf[2] + buf[3] + buf[4] + "\n");
-//        if(buf[0] == 'E'){
-//           Serial.println("E;" + buf[2, 3, 4]);
-//        }else if(buf[0] == 'D'){
-//          Serial.println("D;" + buf[2, 3, 4]);
-//        }
-//    }
-//    
-    //Usando read() 
-//    int valorRecebidoSmartphone = Serial2.read();
-//    if(valorRecebidoSmartphone == '3'){ //conexão estabelecida
-//      digitalWrite(53, HIGH);
-//      delay(100);
-//      digitalWrite(53, LOW);
-//      Serial2.write("4");
-//    }else if(valorRecebidoSmartphone == '6'){ //aviso de inicio da calibragem 
-//      Serial.write("6");
-//    }else if(valorRecebidoSmartphone == '7'){ //calibragem concluida
-//      Serial.write("7");
-//    }else{
-//      Serial.write(valorRecebidoSmartphone);
-//    }
-    
-
-  //Parte de código que funciona com apenas D, E, P
-//    else if(valorRecebidoSmartphone == 68){ //direita -68
-//      Serial.write(valorRecebidoSmartphone);
-//      digitalWrite(53, HIGH);
-//      delay(100);
-//      digitalWrite(53, LOW);
-//      delay(100);
-//      digitalWrite(53, HIGH);
-//      delay(100);
-//      digitalWrite(53, LOW);
-//    }else if(valorRecebidoSmartphone == 69){ //esquerda
-//      Serial.write(valorRecebidoSmartphone);
-//    }else if(valorRecebidoSmartphone == 80){ //parado
-//      Serial.write(valorRecebidoSmartphone);
-//    }
-
-
   }
   
   if(Serial.available()){ 
     //valor recebido da unity    
     int valorRecebidoUnity = Serial.read();
+
+    Serial.print("Recebido da Unity: ");
+    Serial.println(valorRecebidoUnity);
  
     if(valorRecebidoUnity == '0'){ //calibragem direita
       Serial2.write("0");
